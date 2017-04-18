503b013031f0fce2609491dd0aeb50020ab8fafc
Chris <chris@usablehq.com>
2017-04-18 21:05:12 +0100

    Started conversion to es6; added webpack babel-loader

--- src/console-image.js
+++ src/console-image.js
@@ -2,42 +2,41 @@ import toHex from './toHex';
 
 export default function imageToConsole(w,h) {
 
-    var promise = new Promise(function(resolve,reject){
+    let promise = new Promise(function(resolve,reject){
 
-        var img = document.createElement('img');
+        let img = document.createElement('img');
         img.src = 'vangogh.jpg';
-        img.onload = function(){
+        img.addEventListener('load', () => {
 
-            var s = '';
-            var cols = [];
-            var w = w || 40;
-            var h = h || 40;
+            let s = '';
+            let cols = [];
+            let w = w || 40;
+            let h = h || 40;
 
-            var c = document.createElement('canvas');
+            let c = document.createElement('canvas');
                 c.width  = w;
                 c.height = h;
 
-            var ctx = c.getContext('2d');
+            let ctx = c.getContext('2d');
             ctx.fillStyle = 'black';
             ctx.fillRect(0,0,w,h);
 
             ctx.drawImage(img,1,1,w-2,h-2);
 
-            for (var y = 0; y < h; y++) {
+            for (let y = 0; y < h; y++) {
                 if (y > 0) { s += '\n'; }
-                for (var x = 0; x < w; x++) {
+                for (let x = 0; x < w; x++) {
                     s += '%c  ';
-                    var col = ctx.getImageData(x, y, 1, 1).data;
-                    var hex = "#" + ("000000" + toHex(col[0], col[1], col[2])).slice(-6);
+                    let col = ctx.getImageData(x, y, 1, 1).data;
+                    let hex = "#" + ("000000" + toHex(col[0], col[1], col[2])).slice(-6);
                     cols.push('background-color:'+hex+';');
                 }
             }
 
             cols.unshift(s);
-
             resolve(cols);
 
-        }
+        });
 
     });
 
