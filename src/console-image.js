48d91a0a1cb8154b2c5aeb5d458ce300b5d55c75
Chris <chris@usablehq.com>
2017-04-19 20:53:21 +0100

    Moved image loader outside of module

--- src/console-image.js
+++ src/console-image.js
@@ -1,42 +1,33 @@
 import toHex from './toHex';
 
-export default function imageToConsole(w,h) {
+export default function imageToConsole(img, w, h) {
 
     let promise = new Promise(function(resolve,reject){
 
-        let img = document.createElement('img');
-        img.src = 'vangogh.jpg';
-        img.addEventListener('load', () => {
+        let s = '';
+        let cols = [];
 
-            let s = '';
-            let cols = [];
-            let w = w || 40;
-            let h = h || 40;
+        let c = document.createElement('canvas');
+            c.width  = w;
+            c.height = h;
 
-            let c = document.createElement('canvas');
-                c.width  = w;
-                c.height = h;
-
-            let ctx = c.getContext('2d');
+        let ctx = c.getContext('2d');
             ctx.fillStyle = 'black';
             ctx.fillRect(0,0,w,h);
-
             ctx.drawImage(img,1,1,w-2,h-2);
 
-            for (let y = 0; y < h; y++) {
-                if (y > 0) { s += '\n'; }
-                for (let x = 0; x < w; x++) {
-                    s += '%c  ';
-                    let col = ctx.getImageData(x, y, 1, 1).data;
-                    let hex = "#" + ("000000" + toHex(col[0], col[1], col[2])).slice(-6);
-                    cols.push('background-color:'+hex+';');
-                }
+        for (let y = 0; y < h; y++) {
+            if (y > 0) { s += '\n'; }
+            for (let x = 0; x < w; x++) {
+                s += '%c  ';
+                let col = ctx.getImageData(x, y, 1, 1).data;
+                let hex = "#" + ("000000" + toHex(col[0], col[1], col[2])).slice(-6);
+                cols.push('background-color:'+hex+';');
             }
+        }
 
-            cols.unshift(s);
-            resolve(cols);
-
-        });
+        cols.unshift(s);
+        resolve(cols);
 
     });
 
